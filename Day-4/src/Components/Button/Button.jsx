db5620bcefbec07920de5aaeecfc17046b9e0833
Ilhan Madni <madniilhan@gmail.com>
2026-06-17 22:13:59 +0530

    updating Day-4

---
+++ Day-4/src/Components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <>
+     <div className={styles.btn}>
+        this is a button
+     </div>
+    
+    </>
+  )
+}
+
+export default Button
---
+++ Day-4/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Headers.module.css'
+
+const Header = () => {
+  return (
+  <>
+   
+   <div className={styles.headers}>
+     <h3>Ilhan Madni</h3>
+     <button className={styles.btn}>Login</button>
+
+
+   </div>
+  
+  </>
+  )
+}
+
+export default Header
